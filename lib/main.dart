c3ac8eb21bee7366bd5522ee78916ca8394cf8f9
Aekalaivan <aekalaivanpn@gmail.com>
2021-12-16 03:13:58 +0530

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -40,106 +40,125 @@ class _LoginPageState extends State<LoginPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Container(
-              padding: EdgeInsets.fromLTRB(120.0, 50.0, 120.0, 0.0),
-              child: CircleAvatar(
-                radius: 65,
-                backgroundImage: NetworkImage('https://cdn.designcrowd.com/blog/2015/December/top-100-brands-theme-blue-logos/12_HP_400.png'),
-                backgroundColor: Colors.white,
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.fromLTRB(25.0, 50.0, 25.0, 0.0),
-              child: Column(
-                children: [
-                  TextField(
-                    decoration: InputDecoration(
-                      labelText: 'USERNAME',
-                      labelStyle: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.grey,
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 25.0),
-                  TextField(
-                    decoration: InputDecoration(
-                      labelText: 'PASSWORD',
-                      labelStyle: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.grey,
-                      ),
-                    ),
-                    obscureText: true,
-                  ),
-                  SizedBox(height: 10),
-                  Container(
-                    alignment: Alignment(1.0, 0.0),
-                    padding: EdgeInsets.only(left: 20.0, top: 15.0),
-                    child: InkWell(
-                      child: Text(
-                        'Forgot Password',
-                        style: TextStyle(
-                          color: Colors.blue,
-                          fontWeight: FontWeight.bold,
-                          decoration: TextDecoration.underline,
-                        ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 40),
-                  Container(
-                    height: 50.0,
-                    child: Material(
-                      borderRadius: BorderRadius.circular(20.0),
-                      shadowColor: Colors.blueAccent,
-                      color: Colors.blue,
-                      elevation: 8.0,
-                      child: GestureDetector(
-                        onTap: () {
-                          Navigator.pushNamed(context, '/home');
-                        },
-                        child: Center(
-                          child: Text(
-                            'LOGIN',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 17.5,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 40),
-                  Container(
-                    height: 50.0,
-                    child: Material(
-                      borderRadius: BorderRadius.circular(20.0),
-                      shadowColor: Colors.blueAccent,
-                      color: Colors.blue,
-                      elevation: 8.0,
-                      child: GestureDetector(
-                        onTap: () {},
-                        child: Center(
-                          child: Text(
-                            'SIGN UP',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 17.5,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            _CompanyLogo(),
+            _LoginDetails(),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class _CompanyLogo extends StatelessWidget {
+  const _CompanyLogo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(120.0, 50.0, 120.0, 0.0),
+      child: CircleAvatar(
+        radius: 65,
+        backgroundImage: NetworkImage('https://cdn.designcrowd.com/blog/2015/December/top-100-brands-theme-blue-logos/12_HP_400.png'),
+        backgroundColor: Colors.white,
+      ),
+    );
+  }
+}
+
+class _LoginDetails extends StatelessWidget {
+  const _LoginDetails({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+      padding: EdgeInsets.fromLTRB(25.0, 50.0, 25.0, 0.0),
+      child: Column(
+        children: [
+          TextField(
+            decoration: InputDecoration(
+              labelText: 'USERNAME',
+              labelStyle: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.grey,
+              ),
+            ),
+          ),
+          SizedBox(height: 25.0),
+          TextField(
+            decoration: InputDecoration(
+              labelText: 'PASSWORD',
+              labelStyle: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.grey,
+              ),
+            ),
+            obscureText: true,
+          ),
+          SizedBox(height: 10),
+          Container(
+            alignment: Alignment(1.0, 0.0),
+            padding: EdgeInsets.only(left: 20.0, top: 15.0),
+            child: InkWell(
+              child: Text(
+                'Forgot Password',
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontWeight: FontWeight.bold,
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 40),
+          Container(
+            height: 50.0,
+            child: Material(
+              borderRadius: BorderRadius.circular(20.0),
+              shadowColor: Colors.blueAccent,
+              color: Colors.blue,
+              elevation: 8.0,
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.pushNamed(context, '/home');
+                },
+                child: Center(
+                  child: Text(
+                    'LOGIN',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17.5,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 40),
+          Container(
+            height: 50.0,
+            child: Material(
+              borderRadius: BorderRadius.circular(20.0),
+              shadowColor: Colors.blueAccent,
+              color: Colors.blue,
+              elevation: 8.0,
+              child: GestureDetector(
+                onTap: () {},
+                child: Center(
+                  child: Text(
+                    'SIGN UP',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17.5,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
