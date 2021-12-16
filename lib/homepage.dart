2531e6519c88f8bed1d69b08c98f1eb4b8f420d4
Aekalaivan <aekalaivanpn@gmail.com>
2021-12-16 09:18:03 +0530

    Fourth Commit

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -11,16 +11,45 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.fromLTRB(15.0, 25.0, 25.0, 0.0),
-        alignment: Alignment(0.0, 0.0),
-        child: Text('Home Page',
-          style: TextStyle(
-            fontSize: 40.0,
-            fontWeight: FontWeight.bold,
-            color: Colors.blue,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+            child: Text('Home Page',
+              style: TextStyle(
+                fontSize: 40.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.blueAccent,
+                decoration: TextDecoration.underline,
+              ),
+            ),
           ),
-        ),
+          SizedBox(height: 500),
+          Container(
+            child: GestureDetector(
+              onTap: () {},
+              child: Container(
+                height: 50.0,
+                child: Material(
+                borderRadius: BorderRadius.circular(20.0),
+                shadowColor: Colors.blueAccent,
+                color: Colors.blue,
+                elevation: 8.0,
+                  child: Center(
+                    child: Text(
+                      'LOGOUT',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17.5,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
